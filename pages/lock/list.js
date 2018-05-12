834c436da9832b07a94ed96f42642f916b2eff77
DengPan <571143755@qq.com>
2018-05-12 18:05:55 +0800

    05121805

--- pages/lock/list.js
+++ pages/lock/list.js
@@ -1,3 +1,57 @@
+var lockList = [];
+var write36F5 = '';
+var notify36F6 = '';
+var deviceId = '';
+var serviceId = '';
+var _this = "";
+var companyId = [1, 2];
+const app = getApp()
+//查看蓝牙适配器状态
+const getStatus = function () {
+  wx.getBluetoothAdapterState({
+    success: function (res) {
+      console.log(res)
+      if (res.discovering == true) {
+
+      }
+    },
+    fail: function (res) {
+      console.log(res)
+    }
+  })
+}
+//当有设备被扫描到时触发
+const onDeviceFound = function (devices) {
+  console.log('new device list has founded')
+  console.log(devices)
+  //判断是否是该公司的蓝牙设备
+  var resultBuffer = devices.devices[0].advertisData
+  var resultBytes = new Int8Array(resultBuffer)
+  console.log(resultBytes)
+  if (resultBytes[0] == companyId[0] && resultBytes[1] == companyId[1]) {
+    var newEquipment = {
+      "MAC": devices.devices[0].deviceId,
+      "lock_name": devices.devices[0].name,
+      "status": "0"
+    }
+    lockList.push(newEquipment)
+    _this.setData({
+      lockList:lockList
+    });
+  }
+}
+//扫描蓝牙设备
+const startDiscovery = function () {
+  wx.startBluetoothDevicesDiscovery({
+    allowDuplicatesKey: false,
+    fail: function (res) {
+      console.log(res)
+    },
+    success: function (res) {
+      console.log(res)
+    }
+  })
+}
 Page({
 
   /**
@@ -7,130 +61,90 @@ Page({
     hideHeader: true,
     hideBottom: true,
     refreshTime: '',
-    locklist: [{
-      "MAC": "1A:22:5F:33:22:4A",
-      "lock_name": "一号锁",
-      "status": "0",
-      "id": 1525659394339
-    }, {
-      "MAC": "1A:22:5F:33:22:4A",
-      "lock_name": "二号锁",
-      "status": "0",
-      "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }, {
-        "MAC": "1A:22:5F:33:22:4A",
-        "lock_name": "二号锁",
-        "status": "0",
-        "id": 1525659394340
-      }],
+    topText: {
+      "lock_name": "锁名",
+      "operate": "连接状态"
+    },
+    lockList: lockList,
     allPages: '',
     currentPage: 1,
     loadMoreData: '加载更多...'
   },
-
+  //扫描设备
+  discoveryRequipment: function () {
+    _this = this;
+    console.log('discoveryRequipment')
+    wx.openBluetoothAdapter({
+      success: function (res) {
+        console.log(res)
+        wx.onBluetoothDeviceFound(onDeviceFound)
+        startDiscovery()
+      },
+      fail: function (res) {
+        console.log(res)
+        if (res.errCode == 10001) {
+          console.log('blue tooth not open or enable')
+        }
+        wx.showToast({
+          title: "蓝牙未打开",
+          icon: "none"
+        })
+      }
+    })
+  },
   /**
-   * 生命周期函数--监听页面加载
-   */
+  * 生命周期函数--监听页面加载
+  */
   onLoad: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
